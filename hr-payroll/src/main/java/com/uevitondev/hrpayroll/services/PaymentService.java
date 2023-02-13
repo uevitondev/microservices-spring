8500540b1393533e465819801ba7fd3e90f82d9c
uevitondev <uevitonsantos@outlook.com>
2023-02-13 19:59:22 -0300

    Feign client

--- hr-payroll/src/main/java/com/uevitondev/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/uevitondev/hrpayroll/services/PaymentService.java
@@ -2,28 +2,18 @@ package com.uevitondev.hrpayroll.services;
 
 import com.uevitondev.hrpayroll.entities.Payment;
 import com.uevitondev.hrpayroll.entities.Worker;
+import com.uevitondev.hrpayroll.feignclients.WorkerFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class PaymentService {
 
     @Autowired
-    private RestTemplate restTemplate;
-
-    @Value("${hr-worker.host}")
-    private String workerHost;
+    private WorkerFeignClient workerFeignClient;
 
     public Payment getPayment(long workerId, int days) {
-        Map<String, String> uriVariables = new HashMap<>();
-        uriVariables.put("id", String.valueOf(workerId));
-
-        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+        Worker worker = workerFeignClient.findById(workerId).getBody();
         return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 
