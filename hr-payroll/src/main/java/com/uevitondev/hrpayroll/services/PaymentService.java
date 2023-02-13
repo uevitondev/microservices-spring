555bf13de7f7f3a44e5f3019ffca0f70960c6a92
uevitondev <uevitonsantos@outlook.com>
2023-02-13 18:28:27 -0300

    Rest template

--- hr-payroll/src/main/java/com/uevitondev/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/uevitondev/hrpayroll/services/PaymentService.java
@@ -1,13 +1,30 @@
 package com.uevitondev.hrpayroll.services;
 
 import com.uevitondev.hrpayroll.entities.Payment;
+import com.uevitondev.hrpayroll.entities.Worker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PaymentService {
 
-    public Payment getPayment(long workerId, int days){
-        return new Payment("Bob",200.0, days);
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Value("${hr-worker.host}")
+    private String workerHost;
+
+    public Payment getPayment(long workerId, int days) {
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("id", String.valueOf(workerId));
+
+        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+        return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 
 }
