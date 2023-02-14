7345aad16d05fd7afb76064b83d799c47d326dc7
uevitondev <uevitonsantos@outlook.com>
2023-02-14 18:36:48 -0300

    Configuring Zuul api gateway

--- hr-api-gateway-zuul/src/main/java/com/uevitondev/hrapigatewayzuul/HrApiGatewayZuulApplication.java
+++ hr-api-gateway-zuul/src/main/java/com/uevitondev/hrapigatewayzuul/HrApiGatewayZuulApplication.java
@@ -2,12 +2,16 @@ package com.uevitondev.hrapigatewayzuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableEurekaClient
+@EnableZuulProxy
 @SpringBootApplication
 public class HrApiGatewayZuulApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HrApiGatewayZuulApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HrApiGatewayZuulApplication.class, args);
+    }
 
 }
