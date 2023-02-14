85662560028fe068a3bb2928fa3f2e27ef059344
uevitondev <uevitonsantos@outlook.com>
2023-02-14 14:25:59 -0300

    Ribbon load balancing

--- hr-worker/src/main/java/com/uevitondev/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/uevitondev/hrworker/resources/WorkerResource.java
@@ -2,7 +2,10 @@ package com.uevitondev.hrworker.resources;
 
 import com.uevitondev.hrworker.entities.Worker;
 import com.uevitondev.hrworker.repositories.WorkerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,11 +13,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/workers")
 public class WorkerResource {
+
+    private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+
+    @Autowired
+    private Environment environment;
 
     @Autowired
     private WorkerRepository workerRepository;
@@ -27,6 +34,9 @@ public class WorkerResource {
 
     @GetMapping("/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id) {
+
+        logger.info("Port = "+ environment.getProperty("local.server.port"));
+
         Worker worker = workerRepository.findById(id).get();
         return ResponseEntity.ok(worker);
     }
